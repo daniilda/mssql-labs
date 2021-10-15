4434e43be48c440558d53fddd7edc2473b521f5b
DanielK <m2002725@edu.misis.ru>
2021-10-15 23:17:28 +0300

    Renaming + Update Lab3

--- Lab3.sql
+++ Lab3.sql
@@ -13,14 +13,14 @@ inner join SalesLT.Address as A on CA.AddressID = A.AddressID
 where CA.AddressType = 'Main Office'
 -------------------------------------------------------------------------
 -------------------------------------------------------------------------
-select CompanyName, FirstName, LastName, SalesOrderID, TotalDue
+select C.CompanyName, C.FirstName, C.LastName, SOH.SalesOrderID, SOH.TotalDue
 from SalesLT.SalesOrderHeader as SOH
     right join SalesLT.Customer as C on C.CustomerID = SOH.CustomerID
 order by TotalDue DESC;
 -------------------------------------------------------------------------
-select C.CustomerID, CompanyName, FirstName, LastName, Phone, AddressID
+select C.CustomerID, C.CompanyName, C.FirstName, C.LastName, C.Phone, CA.AddressID
 from SalesLT.Customer as C
-left join SalesLT.CustomerAddress on C.CustomerID = CustomerAddress.CustomerID
+left join SalesLT.CustomerAddress as CA on C.CustomerID = CA.CustomerID
 where AddressID is null
 -------------------------------------------------------------------------
 select C.CustomerID, SOD.ProductID
@@ -28,12 +28,19 @@ from SalesLT.Customer as C
 left outer join SalesLT.SalesOrderHeader as SOH on SOH.CustomerID = C.CustomerID
 left outer join SalesLT.SalesOrderDetail as SOD on SOD.SalesOrderID = SOH.SalesOrderID
 where ProductID is null
---
-select P.ProductID, CustomerID
+union
+select SOH.CustomerID, P.ProductID
 from SalesLT.Product as P
 left outer join SalesLT.SalesOrderDetail as SOD on SOD.ProductID = P.ProductID
 left join SalesLT.SalesOrderHeader as SOH on SOD.SalesOrderID = SOH.SalesOrderID
 where SOD.SalesOrderID is null
+--------------------------------------------------------------------------
+select P.ProductID, c.CustomerID
+from SalesLT.Customer as c
+full outer join SalesLT.SalesOrderHeader as SOH on SOH.CustomerID = c.CustomerID
+full outer join SalesLT.SalesOrderDetail as SOD on SOD.SalesOrderID = SOH.SalesOrderID
+full outer join SalesLT.Product as P on SOD.ProductID = P.ProductID
+where SOH.SalesOrderID is null
 --------------------------------------------------------------------------
 --------------------------------------------------------------------------
 --------------------------------------------------------------------------

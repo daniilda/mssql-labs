37059f96fcde3bb7c6ba65779eb28d1921e2d654
DanielK <m2002725@edu.misis.ru>
2021-10-21 10:26:20 +0300

    added lab4

---
+++ lab4.sql
@@ -0,0 +1,43 @@
+-------------------------------------------------------------------------
+-- Лабораторная 4  -- Кузнецов Д.А -- БИВТ-20-7 -------------------------
+-------------------------------------------------------------------------
+select  C.CompanyName, A.AddressLine1, A.City, 'Shipping' as AddressType
+from SalesLT.Customer C
+inner join SalesLT.CustomerAddress CA on CA.CustomerID = C.CustomerID
+inner join SalesLT.Address A on A.AddressID = CA.AddressID
+where CA.AddressType = 'Shipping'
+union
+select C.CompanyName, A.AddressLine1, A.City, 'Billing' as AddressType
+from SalesLT.Customer C
+inner join SalesLT.CustomerAddress CA on CA.CustomerID = C.CustomerID
+inner join SalesLT.Address A on A.AddressID = CA.AddressID
+where CA.AddressType = 'Main Office'
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+select  C.CompanyName
+from SalesLT.Customer C
+inner join SalesLT.CustomerAddress CA on CA.CustomerID = C.CustomerID
+inner join SalesLT.Address A on A.AddressID = CA.AddressID
+where CA.AddressType = 'Main Office'
+except
+select  C.CompanyName
+from SalesLT.Customer C
+inner join SalesLT.CustomerAddress CA on CA.CustomerID = C.CustomerID
+inner join SalesLT.Address A on A.AddressID = CA.AddressID
+where CA.AddressType = 'Shipping'
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+select  C.CompanyName
+from SalesLT.Customer C
+inner join SalesLT.CustomerAddress CA on CA.CustomerID = C.CustomerID
+inner join SalesLT.Address A on A.AddressID = CA.AddressID
+where CA.AddressType = 'Main Office'
+intersect
+select  C.CompanyName
+from SalesLT.Customer C
+inner join SalesLT.CustomerAddress CA on CA.CustomerID = C.CustomerID
+inner join SalesLT.Address A on A.AddressID = CA.AddressID
+where CA.AddressType = 'Shipping'
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------

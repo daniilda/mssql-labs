7814311c9d05d9a127088d6312dc19420cff6e88
DanielK <m2002725@edu.misis.ru>
2021-12-10 00:58:12 +0300

    lab 10 and 11

---
+++ lab10.sql
@@ -0,0 +1,76 @@
+-------------------------------------------------------------------------
+-- Лабораторная 10  -- Кузнецов Д.А -- БИВТ-20-7 -------------------------
+-------------------------------------------------------------------------
+DECLARE @OrderDate DATE = GETDATE();
+DECLARE @DueDate DATE = DATEADD(d, 7, GETDATE());
+DECLARE @CustomerId INT = 1
+
+BEGIN
+    IF OBJECT_ID('SalesLT.SalesOrderNumber') IS NULL
+        BEGIN
+            CREATE SEQUENCE SalesLT.SalesOrderNumber AS INT
+                START WITH 1 INCREMENT BY 1;
+        END
+
+    DECLARE @NextId INT = NEXT VALUE FOR SalesLT.SalesOrderNumber;
+
+    INSERT INTO SalesLT.SalesOrderHeader (DueDate, OrderDate, CustomerID, ShipMethod)
+    VALUES (@DueDate, @OrderDate, @CustomerId, 'CARGO TRANSPORT 5');
+
+    PRINT (@NextId);
+    PRINT (SCOPE_IDENTITY())
+END
+-------------------------------------------------------------------------------------
+DECLARE @SalesOrderId INT = 71955;
+DECLARE @ProductId INT = 760;
+DECLARE @Quantity INT = 1;
+DECLARE @UnitPrice DECIMAL = 782.99;
+
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderId)
+        PRINT ('ЗАКАЗА НЕТ!') -- THROW 50505, 'СОВСЕМ НЕТ!', 10;
+    ELSE
+        BEGIN
+            INSERT INTO SalesLT.SalesOrderDetail (SalesOrderID, OrderQty, ProductID, UnitPrice)
+            VALUES (@SalesOrderId, @Quantity, @ProductId, @UnitPrice);
+        END;
+END;
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+DECLARE @Categories TABLE
+                    (
+                        ProductCategoryID INT
+                    )
+DECLARE @a          DECIMAL
+DECLARE @b          DECIMAL
+
+BEGIN
+    INSERT INTO @Categories (ProductCategoryID)
+    SELECT ProductCategoryID
+    FROM SalesLT.vGetAllCategories
+    WHERE ParentProductCategoryName = 'Bikes';
+    IF (3000 >
+        (SELECT AVG(ListPrice)
+         FROM SalesLT.Product
+         WHERE Product.ProductCategoryID IN (SELECT * FROM @Categories))) -- Или можно в while условия прописать
+        BEGIN
+            WHILE 1 = 1
+                BEGIN
+                    UPDATE SalesLT.Product
+                    SET ListPrice = ListPrice * 1.1
+                    WHERE ProductCategoryID IN (SELECT * FROM @Categories)
+                    IF ((SELECT MAX(ListPrice)
+                         FROM SalesLT.Product
+                         WHERE Product.ProductCategoryID IN (SELECT * FROM @Categories)) > 8000)
+                        BREAK;
+                END;
+            SET @a = (SELECT AVG(ListPrice)
+                      FROM SalesLT.Product
+                      WHERE Product.ProductCategoryID IN (SELECT * FROM @Categories))
+            PRINT ('NEW AVERAGE PRICE' + CAST(@a as VARCHAR));
+            SET @b = (SELECT MAX(ListPrice)
+                      FROM SalesLT.Product
+                      WHERE Product.ProductCategoryID IN (SELECT * FROM @Categories))
+            PRINT ('NEW MAX PRICE' + CAST(@b as VARCHAR));
+        END;
+END;
---
+++ lab11.sql
@@ -0,0 +1,61 @@
+-------------------------------------------------------------------------
+-- Лабораторная 11  -- Кузнецов Д.А -- БИВТ-20-7 -------------------------
+-------------------------------------------------------------------------
+EXEC sys.sp_addmessage
+     @msgnum = 60000
+    , @severity = 16
+    , @msgtext = N'Order # (%d) does not exist'
+    , @lang = 'us_english';
+GO
+BEGIN
+    DECLARE @SalesOrderID int = 0
+    IF NOT EXISTS(SELECT 1 FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderID)
+        BEGIN
+            DECLARE @Error VARCHAR(100) = FORMATMESSAGE(60000, @SalesOrderID);
+            THROW 60000, @Error, 1
+        END;
+    DELETE FROM SalesLT.SalesOrderDetail WHERE SalesOrderID = @SalesOrderID;
+    DELETE FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderID;
+END;
+-----------------------------------------------------------------------------
+BEGIN
+    BEGIN TRY
+        DECLARE @SalesOrderID int = 0
+        IF NOT EXISTS(SELECT 1 FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderID)
+            BEGIN
+                DECLARE @Error VARCHAR(100) = FORMATMESSAGE(60000, @SalesOrderID);
+                THROW 60000, @Error, 1
+            END;
+        DELETE FROM SalesLT.SalesOrderDetail WHERE SalesOrderID = @SalesOrderID;
+        DELETE FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderID;
+    END TRY
+    BEGIN CATCH
+        PRINT ERROR_MESSAGE()
+    END CATCH
+END;
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+BEGIN
+    DECLARE @SalesOrderID int = 71780
+    BEGIN TRY
+        IF NOT EXISTS(SELECT 1 FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderID)
+            BEGIN
+                DECLARE @Error VARCHAR(100) = FORMATMESSAGE(60000, @SalesOrderID);
+                THROW 60000, @Error, 1
+            END;
+        BEGIN TRANSACTION
+            DELETE FROM SalesLT.SalesOrderDetail WHERE SalesOrderID = @SalesOrderID;
+            THROW 60000, @Error, 1;
+            DELETE FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderID;
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        IF XACT_STATE() != 0
+            BEGIN
+                ROLLBACK TRANSACTION;
+                THROW 60000, 'Aborted transaction', 1;
+            END;
+        ELSE
+            PRINT ERROR_MESSAGE()
+    END CATCH
+END;
